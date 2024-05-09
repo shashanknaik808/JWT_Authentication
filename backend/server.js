4dd130142d1855ad6f723443050802a1f97ee7ff
Shashank Naik <shashanknaik808@gmail.com>
2024-05-09 21:54:33 +0530

    Installed Mongoose

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,8 @@ app.use(express.json);
 app.listen(5000, () => {
     console.log("App started in port no 5000");
 });
+
+
+
+//123456qwerty
+// mongosh "mongodb+srv://cluster0.bbu2htp.mongodb.net/" --apiVersion 1 --username shashanknaik808 --password 123456qwerty
