627390e6976448eff75c85fce940565e98237c3b
Shashank Naik <shashanknaik808@gmail.com>
2024-05-09 22:01:10 +0530

    Connected to MongoDB Atlas cluster

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+Cluster0
+mongoose.connect("mongodb+srv://shashanknaik808:123456qwerty@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 app.use(express.json);
 
@@ -11,4 +14,4 @@ app.listen(5000, () => {
 
 
 //123456qwerty
-// mongosh "mongodb+srv://cluster0.bbu2htp.mongodb.net/" --apiVersion 1 --username shashanknaik808 --password 123456qwerty
+// mongodb+srv://shashanknaik808:<password>@cluster0.bbu2htp.mongodb.net/
