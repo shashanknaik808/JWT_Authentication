bbda18bf518e49b9d3ffe17f4ca5470e00a9fc2f
Shashank Naik <shashanknaik808@gmail.com>
2024-05-09 22:14:14 +0530

    Updated code to properly handle MongoDB connection and Express server initialization

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-import dotenv from "dotenv";
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect("mongodb+srv://shashanknaik808:${process.env.MONGODB_PASSWORD}@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => app.listen(5000))
-    .then(() => console.log("Database is connected! Listening to localhost 5000"))
+mongoose.connect(`mongodb+srv://shashanknaik808:${process.env.MONGODB_PASSWORD}@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        console.log("Database is connected!");
+        app.listen(5000, () => {
+            console.log("App started in port no 5000");
+        });
+    })
     .catch((err) => console.log(err));
 
+app.use(express.json());
 
-app.use(express.json);
-
-app.listen(5000, () => {
-    console.log("App started in port no 5000");
-});
-
-
-
-//123456qwerty
-// mongodb+srv://shashanknaik808:<password>@cluster0.bbu2htp.mongodb.net/
