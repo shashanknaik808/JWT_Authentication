0a7b6eb748ad9888da0f99c3901840727fb0af54
Shashank Naik <shashanknaik808@gmail.com>
2024-05-09 21:36:54 +0530

    Initialized Express server on port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const express = require('express');
+
+const app = express();
+
+app.use(express.json);
+
+app.listen(5000, () => {
+    console.log("App started in port no 5000");
+});
