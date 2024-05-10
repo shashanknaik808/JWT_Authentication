d1369392e408edd29c15cc02770fa792565103f8
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 20:51:01 +0530

    Refined signup and login functions, handling error cases with appropriate HTTP status codes

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,22 +1,22 @@
 const User = require('../model/User.js');
 const bcrypt = require('bcryptjs');
 
-module.exports.signup = async (req, res, next) => {
+module.exports.signup = async function (req, res, next) {
     const { name, email, password } = req.body;
     let existingUser;
     try {
         existingUser = await User.findOne({ email: email });
     }
     catch (err) {
-        console.log(err);
+        return res.status(500).json({ message: "Internal server error while verifying user" });
     }
     if (existingUser) {
-        return res.status(400).json({ message: "User already exists! LOgin Instead" })
+        return res.status(400).json({ message: "User already exists! Login Instead" })
     }
     const hashedPassword = bcrypt.hashSync(password);
     const user = new User({
-        name,
-        email,
+        name: name,
+        email: email,
         password: hashedPassword,
     });
 
@@ -24,8 +24,28 @@ module.exports.signup = async (req, res, next) => {
         await user.save();
     }
     catch (err) {
-        console.log(err);
+        return res.status(501).json({ message: "Internal server while inserting the user" });
     }
     return res.status(201).json({ message: user })
+}
+
+module.exports.login = async function (req, res, next) {
+    const { email, password } = req.body;
+
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    }
+    catch (err) {
+        return res.status(500).json({ message: "Internal server error for old user" });
+    }
+    if (!existingUser) {
+        return res.status(400).json({ message: "User not found. Invalid Email" })
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+        return res.status(401).json({ message: 'Invalid Password' })
+    }
+    return res.status(200).json({ message: 'Successfully Logged In' })
 }
 
