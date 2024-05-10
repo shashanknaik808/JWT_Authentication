b90f480cb71410dd4f8949d0c787c3f783d597d6
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 20:49:10 +0530

    Refactored signup function to check for existing users before creating a new one

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,10 +1,21 @@
 const User = require('../model/User.js');
 
 module.exports.signup = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email: email });
+    }
+    catch (err) {
+        console.log(err);
+    }
+    if (existingUser) {
+        return res.status(400).json({ message: "User already exists! LOgin Instead" })
+    }
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+        name,
+        email,
+        password
     });
 
     try {
@@ -15,3 +26,4 @@ module.exports.signup = async (req, res, next) => {
     }
     return res.status(201).json({ message: user })
 }
+
