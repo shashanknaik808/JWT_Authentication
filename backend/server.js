c0b6599fa9e0023138beb81f12276e2091a384d6
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 20:56:12 +0530

    Fixed server setup by initializing 'app' before using it for listening to port and added comments for clarity

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const router = require("./routes/user_routes.js");
 
-require('dotenv').config();
-
-const app = express();
-
-app.use(express.json());
-app.use('/api', router);
-
 mongoose.connect(`mongodb+srv://shashanknaik808:${process.env.MONGODB_PASSWORD}@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(() => {
-        console.log("Database is connected!");
-        app.listen(5000, () => {
-            console.log("App started in port no 5000");
-        });
-    })
+    .then(() => app.listen(5000))
+    .then(() => console.log("Database is connected! Listening to localhost 5000"))
     .catch((err) => console.log(err));
 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', router);
+
+
+// mongoose.connect(`mongodb+srv://shashanknaik808:${process.env.MONGODB_PASSWORD}@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
