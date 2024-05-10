92b947647fd7a647d88ef50c1746c4bd8f6b05ec
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 21:12:24 +0530

    Refactored server setup to use environment variables for MongoDB credentials and JWT secret key

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,22 @@ const router = require("./routes/user_routes.js");
 
 require('dotenv').config();
 
-mongoose.connect("mongodb+srv://shashanknaik808:123456qwerty@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => app.listen(5000))
-    .then(() => console.log("Database is connected! Listening to localhost 5000"))
-    .catch((err) => console.log(err));
-
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const JWT_SECRET_KEY = "KEYBOARD_CAT";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', router);
 
-
-// mongoose.connect("mongodb+srv://shashanknaik808:123456qwerty@cluster0.bbu2htp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.hubtpxn.mongodb.net/jwt_aa?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        console.log("Database is connected!");
+        app.listen(5000, () => {
+            console.log("App started in port no 5000");
+        });
+    })
+    .catch((err) => console.log(err));
