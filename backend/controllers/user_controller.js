82a9e85902eacb42f5ed074061e96f3d098d0a5f
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 20:35:18 +0530

    Implemented signup functionality to create a new user in the database

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,17 @@
+const User = require('../model/User.js');
+
+module.exports.signup = async (req, res, next) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        await user.save();
+    }
+    catch (err) {
+        console.log(err);
+    }
+    return res.status(201).json({ message: user })
+}
