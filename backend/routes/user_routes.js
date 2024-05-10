d626d2b9b321949da4828b97b11ac479978b2182
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 21:18:22 +0530

    Configured routes for user signup, login, and user details retrieval with token verification middleware

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signup, login, verifyToken } = require('../controllers/user_controller');
+const { signup, login, getUser } = require('../controllers/user_controller.js');
+const { verifyToken } = require('../middleware/verifyTokenMiddleware.js')
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/user", verifyToken);
+router.get("/user", verifyToken, getUser);
 
 module.exports = router;
