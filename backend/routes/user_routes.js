88e956ecad02008c9de425a53cf53097e9fa7529
Shashank Naik <shashanknaik808@gmail.com>
2024-05-10 21:06:58 +0530

    Added route for verifying JWT token on user requests

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signup, login } = require('../controllers/user_controller');
+const { signup, login, verifyToken } = require('../controllers/user_controller');
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", verifyToken);
 
 module.exports = router;
